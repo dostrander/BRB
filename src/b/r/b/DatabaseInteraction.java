eb25c9497436833bf37c4fd6222bad531fc4b0ed
Jason Mather <jamather@gmail.com>
2012-04-18 23:30:03 -0400

    Databade should now hand queries as well, not sure if case where nothing is found works for searching by child_ids tho, if you call it make sure you do a try catch for null, I will be commenting it to explain tomorrow morning so just check DatabaseInteraction.java for explanation

--- src/b/r/b/DatabaseInteraction.java
+++ src/b/r/b/DatabaseInteraction.java
@@ -5,10 +5,14 @@ package b.r.b;
 
 import android.app.ListActivity;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import static b.r.b.Constants.*;
 
 public class DatabaseInteraction extends ListActivity {
+	private final int ID_COLUMN = 1;
+	private final int MESSAGE_COLUMN = 2;
+	private final int CHILD_IDS_COLUMN = 3;
 	
 	private parentDB parent = new parentDB(this);
 	private StringArrayConverter strc = new StringArrayConverter();
@@ -19,6 +23,37 @@ public class DatabaseInteraction extends ListActivity {
 		values.put(MESSAGE, message);
 		values.put(CHILD_IDS, strc.convertArrayToString(numbers));
 		db.insertOrThrow(PARENT_TABLE, null, values);
+	}
+	
+	public Cursor SearchByMessage(String message){
+		SQLiteDatabase db = parent.getReadableDatabase();
+		
+		return db.query(PARENT_TABLE, new String[] {ID,MESSAGE,CHILD_IDS}, MESSAGE+"=?"
+				, new String[]{message}, null, null, null);
+		
+	}
+	
+	public Cursor SearchById(String id){
+		SQLiteDatabase db = parent.getReadableDatabase();
+		
+		return db.query(PARENT_TABLE, new String[] {ID,MESSAGE,CHILD_IDS},ID+"=?"
+				, new String[]{id}, null, null, null);
+	}
+	
+	public Cursor SearchByChildId(String number){
+		SQLiteDatabase db = parent.getReadableDatabase();
+		
+		Cursor c = db.rawQuery(PARENT_TABLE, null);
+		String s = c.getString(CHILD_IDS_COLUMN);
+		String[] a = strc.convertStringToArray(s);
+
+		for(int i = 0; i < a.length; i ++){
+			if(a[i].equals(number)){
+				return c;
+			}
+		}
+		c.close();
+		return null;
 	}
 	
 }
