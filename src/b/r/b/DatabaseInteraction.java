40f47b5a9b535c817472726c395fce581deec23a
Jason Mather <jamather@gmail.com>
2012-04-19 21:19:12 -0400

    new comments and different searches for child versus parent added

--- src/b/r/b/DatabaseInteraction.java
+++ src/b/r/b/DatabaseInteraction.java
@@ -11,9 +11,9 @@ import static b.r.b.Constants.*;
 
 public class DatabaseInteraction extends ListActivity {
 	//Constants used to access a column using the cursor getString() method later
-	private final int ID_COLUMN = 1;
-	private final int MESSAGE_COLUMN = 2;
-	private final int CHILD_IDS_COLUMN = 3;
+	private final int PID_COLUMN = 1;
+	private final int PARENT_IDS_COLUMN = 4;
+	private final int PCHILD_IDS_COLUMN = 3;
 	//initializing the local objects
 	private parentDB parent = new parentDB(this);
 	private childDB child   = new childDB(this);
@@ -21,8 +21,23 @@ public class DatabaseInteraction extends ListActivity {
 	
 	//to insert a message, you pass the string of the message, and an
 	//array of strings that contain all the numbers it is used for
-	//If it is a universal message I think we should just use null
 	
+	
+	//For inserting into the parent, pass an id, the message, and an array of possible child 
+	//ids
+	public void InsertMessage(String id, String message, String[] cids){
+		SQLiteDatabase db = parent.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put(_ID, id);
+		values.put(MESSAGE, message);
+		values.put(CHILD_IDS, strc.convertArrayToString(cids));
+		
+		db.insertOrThrow(PARENT_TABLE, id, values);
+	}
+	
+	
+	//For inserting into the child, pass the id, an arry of the numbers, the message, and 
+	//an array of the parent ids
 	public void InsertMessage(String id, String[] numbers, String message, int[] pids){
 		String p = "";
 		for(int i = 0; i < pids.length; i ++){
@@ -42,31 +57,22 @@ public class DatabaseInteraction extends ListActivity {
 		
 	}
 	
-	public void InsertMessage(String id, String message, String[] cids){
-		SQLiteDatabase db = parent.getWritableDatabase();
-		ContentValues values = new ContentValues();
-		values.put(_ID, id);
-		values.put(MESSAGE, message);
-		values.put(CHILD_IDS, strc.convertArrayToString(cids));
-		
-		db.insertOrThrow(PARENT_TABLE, id, values);
-	}
 	
 	
 	//to SearchByMessage just pass the String of the message
-	public Cursor SearchByMessage(String message){
-		SQLiteDatabase db = parent.getReadableDatabase();
+	public Cursor SearchChildByMessage(String message){
+		SQLiteDatabase db = child.getReadableDatabase();
 		
-		return db.query(PARENT_TABLE, new String[] {ID,MESSAGE,CHILD_IDS}, MESSAGE+"=?"
+		return db.query(CHILD_TABLE, new String[] {ID,NUMBERS,MESSAGE,PARENT_IDS}, MESSAGE+"=?"
 				, new String[]{message}, null, null, null);
 		
 	}
 	
 	//To search by ID (not sure why you would) just pass the id as a string
-	public Cursor SearchById(String id){
-		SQLiteDatabase db = parent.getReadableDatabase();
+	public Cursor SearchChildById(String id){
+		SQLiteDatabase db = child.getReadableDatabase();
 		
-		return db.query(PARENT_TABLE, new String[] {ID,MESSAGE,CHILD_IDS},ID+"=?"
+		return db.query(CHILD_TABLE, new String[] {ID,NUMBERS,MESSAGE,PARENT_IDS},ID+"=?"
 				, new String[]{id}, null, null, null);
 	}
 	
@@ -74,17 +80,17 @@ public class DatabaseInteraction extends ListActivity {
 	//remember, c may be null so make sure you try catch when you call
 	//also I'm not sure if this will return all the messages with the sent child ID
 	//or just the last one
-	public Cursor SearchByChildId(String number){
-		SQLiteDatabase db = parent.getReadableDatabase();
+	public Cursor SearchChildByParentId(String pid){
+		SQLiteDatabase db = child.getReadableDatabase();
 		
-		Cursor c = db.rawQuery(PARENT_TABLE, null);
-		String s = c.getString(CHILD_IDS_COLUMN);
+		Cursor c = db.rawQuery(CHILD_TABLE, null);
+		String s = c.getString(PARENT_IDS_COLUMN);
 		String[] a = strc.convertStringToArray(s);
 		String[] ids = new String[a.length];
 		
 		for(int i = 0; i < a.length; i ++){
-			if(a[i].equals(number)){
-				ids[i] = c.getString(ID_COLUMN);
+			if(a[i].equals(pid)){
+				ids[i] = c.getString(PID_COLUMN);
 				c = db.query(PARENT_TABLE, new String[] {ID,MESSAGE,CHILD_IDS},ID+"=?"
 						, new String[]{ids[i]}, null,null,null);
 			}
@@ -93,4 +99,45 @@ public class DatabaseInteraction extends ListActivity {
 		return c;
 	}
 	
+	
+	
+	//to SearchByMessage just pass the String of the message
+		public Cursor SearchParentByMessage(String message){
+			SQLiteDatabase db = parent.getReadableDatabase();
+			
+			return db.query(PARENT_TABLE, new String[] {ID,MESSAGE,CHILD_IDS}, MESSAGE+"=?"
+					, new String[]{message}, null, null, null);
+			
+		}
+		
+		//To search by ID (not sure why you would) just pass the id as a string
+		public Cursor SearchParentById(String id){
+			SQLiteDatabase db = parent.getReadableDatabase();
+			
+			return db.query(PARENT_TABLE, new String[] {ID,MESSAGE,CHILD_IDS},ID+"=?"
+					, new String[]{id}, null, null, null);
+		}
+		
+		//To search by childID just pass the number
+		//remember, c may be null so make sure you try catch when you call
+		//also I'm not sure if this will return all the messages with the sent child ID
+		//or just the last one
+		public Cursor SearchParentByChildId(String number){
+			SQLiteDatabase db = parent.getReadableDatabase();
+			
+			Cursor c = db.rawQuery(PARENT_TABLE, null);
+			String s = c.getString(PCHILD_IDS_COLUMN);
+			String[] a = strc.convertStringToArray(s);
+			String[] ids = new String[a.length];
+			
+			for(int i = 0; i < a.length; i ++){
+				if(a[i].equals(number)){
+					ids[i] = c.getString(PID_COLUMN);
+					c = db.query(PARENT_TABLE, new String[] {ID,MESSAGE,CHILD_IDS},ID+"=?"
+							, new String[]{ids[i]}, null,null,null);
+				}
+			}
+			
+			return c;
+		}
 }
