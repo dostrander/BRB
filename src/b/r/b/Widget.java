715df0966b14be7173adb8fe7a1ab3d94253a6d2
Evan Dodge <k2u_r@yahoo.com>
2012-04-23 19:24:45 -0400

    More updates before I'm done for the night - Evan

--- src/b/r/b/Widget.java
+++ src/b/r/b/Widget.java
@@ -17,6 +17,8 @@ import android.widget.Toast;
 public class Widget extends AppWidgetProvider {
 	public static String ACTION_WIDGET_TEXTVIEW = "ActionTextViewWidget";
 	public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
+	public static String ACTION_WIDGET_LEFT = "ActionLeftWidget";
+	public static String ACTION_WIDGET_RIGHT = "ActionRightWidget";
 	public static String NO_MESSAGE = "Enter Text Here...";
 	
 	// Variables
@@ -25,6 +27,8 @@ public class Widget extends AppWidgetProvider {
 	// View
 	RemoteViews remoteViews = new RemoteViews("b.r.b", R.layout.widget);
 	RemoteViews remoteViewsA = new RemoteViews("b.r.b", R.layout.widget);
+	RemoteViews remoteViewsLeft = new RemoteViews("b.r.b", R.layout.widget);
+	RemoteViews remoteViewsRight = new RemoteViews("b.r.b", R.layout.widget);
 	
 	ImageButton enableButton;
 	TextView inputMessage;
@@ -37,20 +41,38 @@ public class Widget extends AppWidgetProvider {
     	prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
     	editor = prefs.edit();
     	
+    	// Intent for enableButton
     	Intent active = new Intent(context, Widget.class);
     	active.setAction(ACTION_WIDGET_RECEIVER);
     	
     	PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context, 0, active, 0);
     	remoteViews.setOnClickPendingIntent(R.id.widget_button, actionPendingIntent);
     	
+    	// Intent for textView
     	Intent textActive = new Intent(context, Widget.class);
     	textActive.setAction(ACTION_WIDGET_TEXTVIEW);
     	
     	PendingIntent textPendingIntent = PendingIntent.getBroadcast(context, 0, textActive, 0);
     	remoteViewsA.setOnClickPendingIntent(R.id.widget_textview, textPendingIntent);
+    	
+    	// Intent for leftArrow
+    	Intent activeLeft = new Intent(context, Widget.class);
+    	activeLeft.setAction(ACTION_WIDGET_LEFT);
+    	
+    	PendingIntent leftPendingIntent = PendingIntent.getBroadcast(context, 0, activeLeft, 0);
+    	remoteViewsLeft.setOnClickPendingIntent(R.id.widget_left_button, leftPendingIntent);
+    	
+    	// Intent for rightArrow
+    	Intent activeRight = new Intent(context, Widget.class);
+    	activeRight.setAction(ACTION_WIDGET_RIGHT);
+    	
+    	PendingIntent rightPendingIntent = PendingIntent.getBroadcast(context, 0, activeRight, 0);
+    	remoteViewsRight.setOnClickPendingIntent(R.id.widget_right_button, rightPendingIntent);
 
     	appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
     	appWidgetManager.updateAppWidget(appWidgetIds, remoteViewsA);
+    	appWidgetManager.updateAppWidget(appWidgetIds, remoteViewsLeft);
+    	appWidgetManager.updateAppWidget(appWidgetIds, remoteViewsRight);
     }
    
     @Override
@@ -103,11 +125,15 @@ public class Widget extends AppWidgetProvider {
     	    	}
     		}
     		else if (intent.getAction().equals(ACTION_WIDGET_TEXTVIEW)) {
-    			String msg2 = "TextView Selected";
-    			
-    			Toast.makeText(context, msg2, Toast.LENGTH_SHORT).show();
-
+    			Toast.makeText(context, "TextView Selected", Toast.LENGTH_SHORT).show();
     		}
+    		else if (intent.getAction().equals(ACTION_WIDGET_LEFT)) {
+    			Toast.makeText(context, "Left", Toast.LENGTH_SHORT).show();
+    		}
+    		else if (intent.getAction().equals(ACTION_WIDGET_RIGHT)){
+    			Toast.makeText(context, "Right", Toast.LENGTH_SHORT).show();
+    		}
+    		
     		super.onReceive(context, intent);
     	}
     }
