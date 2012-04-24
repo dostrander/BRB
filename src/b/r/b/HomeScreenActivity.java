92aa95fa16f890f2c43ff0f6ecad30b199e3283a
Evan Dodge <k2u_r@yahoo.com>
2012-04-24 17:20:45 -0400

    whatever

--- src/b/r/b/HomeScreenActivity.java
+++ src/b/r/b/HomeScreenActivity.java
@@ -142,7 +142,6 @@ public class HomeScreenActivity extends TabActivity {
     public void onStart(){
     	super.onStart();
     	Log.d(TAG,"in onStart");
-    	Toast.makeText(this, String.valueOf(getSharedPreferences(PREFS,MODE_PRIVATE).getInt(MESSAGE_ENABLED_KEY, NO_MESSAGE_SELECTED)), Toast.LENGTH_LONG).show();
     	
     	int db_id = getSharedPreferences(PREFS,MODE_PRIVATE).getInt(DB_ID_KEY, -1);
     	if(isEnabled() == MESSAGE_ENABLED && db_id >= 0){
