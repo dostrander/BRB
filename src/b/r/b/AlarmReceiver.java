ca5f5e379f3b1a42f324dbbe360591d3e95822cc
Max Schwenk <maschwenk@gmail.com>
2012-05-06 15:29:16 -0400

    commented alarm receiver

--- src/b/r/b/AlarmReceiver.java
+++ src/b/r/b/AlarmReceiver.java
@@ -27,20 +27,26 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		
+		//open up the shared preferences
 		SharedPreferences prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
 		SharedPreferences.Editor editor = prefs.edit();
 		
+		/* update the shared preferences to reflect that BRB has been
+		 * disabled.
+		 */
 		editor.putInt(MESSAGE_ENABLED_KEY, MESSAGE_DISABLED);
 		editor.commit();
 	
-		// Update the widget
+		/* Update the widget, changing the color of the button to reflect that
+		*  BRB has been disabled.
+		*/
     	AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
     	RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
     	ComponentName thisWidget = new ComponentName(context, Widget.class);
     	remoteViews.setImageViewResource(R.id.widget_button, R.drawable.enabled_message_selector);
     	appWidgetManager.updateAppWidget(thisWidget, remoteViews);
-		
+    	
+		//inform the user that BRB has been disabled because of alarm
 		Toast.makeText(context, "BRB Disabled From Preset End Time", Toast.LENGTH_LONG).show();
 	}
 }
